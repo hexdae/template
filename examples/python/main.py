5189e3c27a09a6ce191365156dadcc78e56ef23f
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2024-04-22 17:48:04 -0400

    [OCI] adapt for python

--- examples/python/main.py
+++ examples/python/main.py
@@ -1,10 +1,12 @@
 import examples.python.library as library
+import numpy as np
 
 
 def main():
     """Main function."""
     print("Hello World! Python:", library.function())
     print("Sum:", 3 + 3)
+    print("numpy", np.array([1, 2, 3]))
 
 
 if __name__ == "__main__":
